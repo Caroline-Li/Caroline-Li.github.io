0a9f0b6f604f17df5f927575fc00e9cc11c7464d
Caroline Li <caroline.li.atx@gmail.com>
2020-07-10 16:50:49 -0500

    added sensor page with javascript integration with sensor api

---
+++ assets/js/scripts.js
@@ -0,0 +1,30 @@
+function sensorRequest() {
+// Create request variable and assign new XMLHttpRequest object
+console.log("hello")
+var request = new XMLHttpRequest()
+
+request.open('GET', 'https://kv0obds3p6.execute-api.us-east-2.amazonaws.com/read_dynamoDB', true)
+
+request.onload = function () {
+// Begin accessing JSON data
+    var data = JSON.parse(this.response)
+
+    if (request.status >= 200 && request.status < 400)
+    {
+        data.Items.forEach((sensor) => {
+        var sensorData = sensor.Sensor
+        console.log(sensorData)
+        })
+        var latest = data.Items[0].Sensor
+        document.getElementById('textField').value = latest.toString() + "°F"
+    }
+    else
+    {
+        console.log('error')
+    }
+}
+
+request.send()
+}
+
+
